ca6d7c475c307fa36db2d3e6b63ae68ebe4c863f
Ashutosh <omkarashu28@gmail.com>
2023-03-27 15:06:26 +0530

    Biuld  Navbar and started working on footer.

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "../Components/Footer";
+import HeroSection from "../Components/HeroSection";
+import NewsLetter from "../Components/NewsLetter";
+import Products from "../Components/Products";
+
+function Home() {
+  return (
+    <>
+      {/* <HeroSection />
+      <Products />
+      <NewsLetter />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default Home;
